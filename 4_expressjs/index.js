e2ad570d4d194101520bab99a1cf711700af5044
NAVEEN KUMAR P <135513766+naveenkumar-42@users.noreply.github.com>
2024-07-14 18:26:29 +0530

    index.js

--- 4_expressjs/index.js
+++ 4_expressjs/index.js
@@ -4,6 +4,5 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World -7376222IT210 -NAVEEN KUMAR P'))
 app.get('/naveenkumar', (req, res) => res.send('7376222IT210 -NAVEEN KUMAR P'))
-app.get('/manikandan', (req, res) => res.send('7376222IT192 -MANIKANDAN G'))
-
+app.get('/naveenkumar', (req,res) => res.send('Im Learning Express.js'))
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
